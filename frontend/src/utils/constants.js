894f896fa456f59cfd78d10112dd3e291e5b865c
itamar reiter <itamarreiter@gmail.com>
2022-11-09 23:03:08 +0200

    change url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const apiData = {
-  baseUrl: "www.itamarreiter.students.nomoredomainssbs.ru",
+  baseUrl: "https://api.itamarreiter.students.nomoredomainssbs.ru",
 };
 
  /* token from practicum "4c5ffc10-7ef8-4216-9730-733ca7b03273" */
