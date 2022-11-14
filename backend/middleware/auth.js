86b876602bb6c0eede7d7babb205589e8c5852c8
itamar reiter <itamarreiter@gmail.com>
2022-11-14 11:11:19 +0200

    changed some auth middleware content

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ const { UnauthorizedError } = require('../utils/errors');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    console.log('through auth middleware')
+    console.log("through auth middleware, no authorization or authorization isn't JWT")
     throw new UnauthorizedError('Authorization required');
   }
   const token = authorization.replace('Bearer ', '');
@@ -12,9 +12,10 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'not-so-secret-string');
   } catch (err) {
-    console.log('through auth middleware')
+    console.log('through auth middleware, jwt payload not loaded')
     next(new UnauthorizedError('Authorization required'));
   }
   req.user = payload;
+  console.log(req.user);
   next();
 };
